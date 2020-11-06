c4f918dac985475b621c355c1fef48ba1a349181
oOBoomberOo <boombercmd123@gmail.com>
2020-11-06 18:10:15 +0700

    Added custom theme: #3

--- script/player_controller.gd
+++ script/player_controller.gd
@@ -8,15 +8,19 @@ signal landed
 var run_speed = 300
 var jump_speed = 800
 
-var backflip_threshold = jump_speed / 2
-var running_threshold = run_speed / 2
+var backflip_threshold = jump_speed / 1.5
+var running_threshold = 0.1
 
+var bounce = false
 var gravity = 20
 var motion = Vector2()
 
 onready var animator: AnimatedSprite = $AnimatedSprite
 
-func _physics_process(delta):
+func _ready() -> void:
+	animator.playing = true
+
+func _physics_process(_delta):
 	gather_input()
 	gather_environment()
 	motion = move_and_slide(motion, Vector2.UP)
@@ -27,27 +31,34 @@ func gather_input():
 	motion.x = 0
 	
 	if Input.is_action_pressed("move_left"):
-		motion.x -= 1
+		move(-1)
 	if Input.is_action_pressed("move_right"):
-		motion.x += 1
-	
-	motion.x *= run_speed
+		move(1)
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump()
+		jump(1)
 
 func gather_environment():
 	motion.y += gravity
 
-func moving_direction(epoch = 0.1):
+func moving_direction(epoch := 0.1):
 	if motion.x < -epoch:
 		animator.flip_h = true
 	elif motion.x > epoch:
 		animator.flip_h = false
 
-func jump():
-	motion.y -= jump_speed
+func launch(power: float) -> void:
+	motion.y = 0
+	bounce = true
+	jump(power)
+
+func jump(power: float) -> void:
+	motion.y -= power * jump_speed
 	emit_signal("jumped")
+	
+func move(dx: float) -> void:
+	motion.x += dx * run_speed
 
 func land():
+	bounce = false
 	emit_signal("landed")
